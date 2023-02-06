87ac4400ee72047907378eb40889a6dc43e8e86f
macyenga <macyenga@gmail.com>
2023-02-06 13:19:34 +0200

    client coedit

--- server/src/services/users.ts
+++ server/src/services/users.ts
@@ -49,7 +49,7 @@ export const sendActivationCode = (user: User): void => {
 		'Activation code for imibonano-account',
 		`<h1>Hi and thanks for signing up!</h1>
 			<p>Please visit the link to activate your account here:</p>
-			<a href='https://matcha-client.onrender.com/login?activate=${user.activationCode}'>Link</a>
+			<a href='https://macyenga.github.io/imibonano/login?activate=${user.activationCode}'>Link</a>
 			<p> See you at imibonano! <3 </p>`
 	);
 };
@@ -146,7 +146,7 @@ export const mailEmailUpdateLink = (email: User['email'], newUpdateRequest: Emai
 		'Confirm email reset for Imibonano-account',
 		`<h1>Hi, here you can confirm email reset!</h1>
 			<p>Visit the link below to reset your email:</p>
-			<a href='https://imibonano-client.onrender.com/update_email?update=${newUpdateRequest.token}'>Reset email here</a>
+			<a href='https://macyenga.github.io/imibonano/update_email?update=${newUpdateRequest.token}'>Reset email here</a>
 			<p>Link will be active until ${newUpdateRequest.expiresAt}.</p>
 			<p>Ignore this message if you haven't requested email reset.</p>
 
