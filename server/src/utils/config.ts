b6cfeb49517fef55cabfeb60d2dc8e8c70e12cf5
macyenga <macyenga@gmail.com>
2023-03-16 05:00:16 +0200

    To configure process.env in  environment !

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -1,10 +1,10 @@
 // const DB_PORT: number = process.env.NODE_ENV === 'test' ? 5433 : 5432;
 
-// const DB_NAME: string = process.env.NODE_ENV === 'test' ? 'imibonano-test' : 'imibonano';
+// const DB_NAME: string = process.env.NODE_ENV === 'test' ? 'matcha-test' : 'matcha';
 
 
-const DB_PORT = Number(5432);
+const DB_PORT = Number(process.env.DB_PORT);
 
-const DB_NAME = String('imibonano_db');
+const DB_NAME = String(process.env.DB_NAME);
 
 export { DB_NAME, DB_PORT };
